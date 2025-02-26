249348c5f08c614285931ef4f5588bff850462e2
jignesh779 <jignesh779@proton.me>
2025-02-26 15:37:56 +0530

    bookreview

---
+++ src/components/Name.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+ Name = (pro) => {
+    return(
+        
+        <>
+        <h1>Name : {pro.fname}</h1>
+        <h1>last Name: {pro.lname}</h1>
+        <h1>phone: {pro.phn}</h1>
+        </>
+    )
+}
+export default Name
